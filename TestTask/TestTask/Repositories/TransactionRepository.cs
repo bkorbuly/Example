a7fdb6be3833da31805f46cefb892066d2e15657
bkorbuly <b.korbuly@gmail.com>
2018-01-25 11:13:49 +0100

    gitingore added

---
+++ TestTask/TestTask/Repositories/TransactionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Entities;
+
+namespace TestTask.Repositories
+{
+    public class TransactionRepository
+    {
+        TransactionContext _transactionContext;
+        public TransactionRepository(TransactionContext transactionContext)
+        {
+            _transactionContext = transactionContext;
+        }
+    }
+}
---
+++ TestTask/TestTask/Services/TransactionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Repositories;
+
+namespace TestTask.Services
+{
+    public class TransactionService
+    {
+        TransactionRepository _transactionRepository;
+
+        public TransactionService(TransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+    }
+}
