5a27fb465392918548276c2216e8a973916b1d49
bkorbuly <b.korbuly@gmail.com>
2018-01-25 12:37:07 +0100

    add transactions from files to db

--- TestTask/TestTask/Repositories/TransactionRepository.cs
+++ TestTask/TestTask/Repositories/TransactionRepository.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TestTask.Entities;
+using TestTask.Models;
 
 namespace TestTask.Repositories
 {
     public class TransactionRepository
     {
         TransactionContext _transactionContext;
+
         public TransactionRepository(TransactionContext transactionContext)
         {
             _transactionContext = transactionContext;
         }
 
-
+        public void Add(Transaction transaction)
+        {
+            _transactionContext.Transactions.Add(transaction);
+            _transactionContext.SaveChanges();
+        }
     }
 }
--- TestTask/TestTask/Services/TransactionService.cs
+++ TestTask/TestTask/Services/TransactionService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using TestTask.Models;
 using TestTask.Repositories;
 
 namespace TestTask.Services
@@ -21,7 +22,13 @@ namespace TestTask.Services
         {
             foreach (string file in Directory.EnumerateFiles(path, "*.csv"))
             {
+                Transaction transaction = new Transaction();
                 string contents = File.ReadAllText(file);
+                string[] seperate = contents.Split(";");
+                transaction.Source = seperate[0];
+                transaction.Destiny = seperate[1];
+                transaction.Amount = seperate[2];
+                _transactionRepository.Add(transaction);
             }
         }
     }
